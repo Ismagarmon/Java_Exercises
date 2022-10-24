32be92341921a194cc4c514065a75c7411876128
Ismael <ismagarmon3@gmail.com>
2022-10-24 12:26:40 +0200

    Exercise_2 Fixed

--- NetBeansProjects/Ejercicio_2/src/ejercicio_2/Ejercicio_2.java
+++ NetBeansProjects/Ejercicio_2/src/ejercicio_2/Ejercicio_2.java
@@ -9,15 +9,18 @@ public class Ejercicio_2 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         String cadena = new String();
-        String n_cadena = new String();
+        int pos = 0;
         do {
-          System.out.println("Introduce un texto: ");
+          System.out.println("Introduce una frase: ");
           cadena = leer.nextLine(); 
-          System.out.println("Introduce un segundo texto: ");
-          n_cadena = leer.nextLine();
-        }while(cadena.isEmpty() || n_cadena.isEmpty());
-        boolean b1=cadena.equals(n_cadena);
-       
-        System.out.println("La frase es la siguiente: "+b1);
+        }while(cadena.isEmpty());
+        cadena = cadena.trim();
+        for (int i = 0; i < (cadena.length()); i++){
+            if (cadena.charAt(i) == ' '){
+                pos = i;
+            }
+        }
+        cadena = cadena.substring(0, pos);
+        System.out.println("La frase resultante es la siguiente: "+cadena);
     }
 }
