67894ee63f4fc1b2beada2eda3e9886df0050b0a
Ismael <ismagarmon3@gmail.com>
2022-10-25 16:21:34 +0200

    ok

--- NetBeansProjects/Ejercicio_1_Arrays/src/ejercicio_1_arrays/Ejercicio_1_Arrays.java
+++ NetBeansProjects/Ejercicio_1_Arrays/src/ejercicio_1_arrays/Ejercicio_1_Arrays.java
@@ -4,9 +4,6 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Ejercicio_1_Arrays {
-
-
-
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("#.00");
