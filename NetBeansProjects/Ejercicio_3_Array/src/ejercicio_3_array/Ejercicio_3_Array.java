e3e2f3870b459be5a901d45b6e24dd279c4d7c8c
Ismael <ismagarmon3@gmail.com>
2022-10-27 16:14:44 +0200

    Exercise_3_Array

---
+++ NetBeansProjects/Ejercicio_3_Array/src/ejercicio_3_array/Ejercicio_3_Array.java
@@ -0,0 +1,41 @@
+
+package ejercicio_3_array;
+import java.util.Scanner;
+//import java.text.DecimalFormat;
+
+public class Ejercicio_3_Array {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        //DecimalFormat df = new DecimalFormat("#.00");
+        int n_alumnos;
+        float cont, nota, suma = 0;
+        double media;
+        
+        System.out.println("Escribir la cantidad de alumnos de la clase: ");
+        n_alumnos = leer.nextInt();
+        cont = n_alumnos;
+        double[] notas = new double[n_alumnos];
+        
+        for(int i = 0;i <= notas.length - 1;i++){
+            System.out.println("Nota del "+(i+1)+"º alumno: ");
+            nota = leer.nextFloat();
+            notas[i] = nota;
+            suma += nota;
+            
+            System.out.println(suma);
+        }
+
+        
+        media = suma/cont;
+        media_alumnos(notas,media);
+    }
+    
+    public static void media_alumnos(double[] notas_alumno, double media_alumnos){
+        for(int j = 0;j <= notas_alumno.length - 1;j++){
+            if(notas_alumno[j] > media_alumnos){
+                System.out.println("El alumno "+(j+1)+" tiene mayor media que el grupo.");
+            }
+        }
+    }
+}
